5d81b16a072864aef79f9a8f31d24d2e80afd8cf
kimseungkyeom <cj3075201@mju.ac.kr>
2025-10-29 04:25:57 +0900

    Delete week6 directory

--- week6/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    console.log("check");
-   $(document).mousemove(function(e){
-      $('#status').html(e.pageX +', '+ e.pageY);
-
-      $(".box").css("width", e.pageX);
-      $(".box").css("width", e.pagey);
-   });
-})
